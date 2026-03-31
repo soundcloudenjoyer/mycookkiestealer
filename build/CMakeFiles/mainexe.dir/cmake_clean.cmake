e42149947ca925222ce29a491649ad4a031094f4
soundcloudenjoyer <soundcloud3umph@gmail.com>
2026-03-31 22:21:05 +0300

    cmake

---
+++ build/CMakeFiles/mainexe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainexe.dir/main.cpp.obj"
+  "CMakeFiles/mainexe.dir/main.cpp.obj.d"
+  "libmainexe.dll.a"
+  "mainexe.exe"
+  "mainexe.exe.manifest"
+  "mainexe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainexe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mainexe.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mainexe.
+# This may be replaced when dependencies are built.
